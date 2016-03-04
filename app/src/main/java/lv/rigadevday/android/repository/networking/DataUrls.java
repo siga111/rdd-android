dc7141d26dd5c9379fb3a68e353655fc4157c669
Sergejs Luhmirins <sergejs.luhmirins@gmail.com>
2016-03-04 22:28:29 +0200

    Changed endpoints to my fork

--- app/src/main/java/lv/rigadevday/android/repository/networking/DataUrls.java
+++ app/src/main/java/lv/rigadevday/android/repository/networking/DataUrls.java
@@ -4,8 +4,8 @@ package lv.rigadevday.android.repository.networking;
  */
 public interface DataUrls {
 
-    String IMAGE_PREFIX = "https://raw.githubusercontent.com/RigaDevDay/RigaDevDay.github.io/master/";
+    String IMAGE_PREFIX = "https://raw.githubusercontent.com/siga111/rdd-android/release2016/data/";
 
     String BASE_URL = "https://raw.githubusercontent.com";
-    String MAIN_FILE_ADDRESS = "/RigaDevDay/RigaDevDay.github.io/master/assets/data/main.json";
+    String MAIN_FILE_ADDRESS = "/siga111/rdd-android/release2016/data/assets/data/main.json";
 }
